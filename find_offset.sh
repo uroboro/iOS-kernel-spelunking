aa9cf40546983009a5465b567f2dd7e827024934
uroboro <uroboro.845@gmail.com>
2017-12-28 03:32:06 -0300

    Simplify logic of find_offst.sh

--- find_offset.sh
+++ find_offset.sh
@@ -2,7 +2,26 @@
 
 scripts/setup.sh
 
-db_file=${1:-deviceDB/iPod7,1_10.txt}
+function make_header() {
+	file="$1"
+	device="$2"
+	version="$3"
+	buildID="$4"
+	path="$5"
+	header=$path/$(basename $file).h
+
+	mkdir -p $path
+	echo "// File automatically created with iOS-kernel-spelunking by uroboro" > $header
+	echo "// Device: $device" >> $header
+	echo "// Version: $version" >> $header
+	echo "// Build: $buildID" >> $header
+	echo >> $header
+	echo "static void setOffsets_${device/,/_}_$buildID(void) {" >> $header
+	scripts/offsets.sh $file | grep define >> $header
+	echo "}" >> $header
+}
+
+db_file=${1:-deviceDB/iOS10/iPod7,1.txt}
 
 IFS=$'\n' read -d '' -r -a devices < $db_file
 lines=$(wc -l $db_file | awk '{ print $1 }')
@@ -13,30 +32,20 @@ for (( i = 0; i < $lines; i++ )); do
 
 	device=$(echo $d | awk '{ print $1 }')
 	version=$(echo $d | awk '{ print $2 }')
-	buildid=$(echo $d | awk '{ print $3 }')
+	buildID=$(echo $d | awk '{ print $3 }')
 	url=$(echo $d | awk '{ print $4 }')
+	ios=$(echo $version | sed 's/\./ /g' | awk '{ print "iOS" $1 }')
 
-	echo "$device on $version ($buildid): $url"
+	echo "$device on $version ($buildID): $url"
 
-	if [[ ! -z "$(ls kernelDB/$device/$buildid/)" ]]; then
-		mkdir -p offsetDB/$device/$buildid
-		for f in kernelDB/$device/$buildid/*; do
-			header=offsetDB/$device/$buildid/$(basename $f).h
-			echo "// File automatically created with iOS-kernel-spelunking by uroboro" > $header
-			echo "// Device: $device" >> $header
-			echo "// Build: $buildid" >> $header
-			echo >> $header
-			scripts/offsets.sh $f | grep define >> $header
-		done
+	if [[ ! -z "$(ls cacheDB/$ios/$device/$buildID/)" ]]; then
+		kaches=$(find cacheDB/$ios/$device/$buildID -type f)
 	else
-		kaches=$(scripts/cache.sh $url | grep kernelcache.release)
-		for f in $kaches; do
-			header=offsetDB/$device/$buildid/$(basename $f).h
-			echo "// File automatically created with iOS-kernel-spelunking by uroboro" > $header
-			echo "// Device: $device" >> $header
-			echo "// Build: $buildid" >> $header
-			echo >> $header
-			scripts/offsets.sh $f | grep define >> $header
-		done
+		kaches=$(scripts/cache.sh $url | tr ' ' '\n' | grep kernelcache.release)
 	fi
+
+	offsetDBPath=offsetDB/$ios/$device/$buildID
+	for f in $kaches; do
+		echo make_header $f $device $version $buildID $offsetDBPath
+	done
 done
